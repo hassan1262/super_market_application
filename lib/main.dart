d855720a89b8814f2f3bb6d00fc66506682c3fc8
Muhammad Tarek <68518117+MuhammadTarekk@users.noreply.github.com>
2021-12-16 18:11:37 +0200

    d

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:super_market_application/screens/edit_profile.dart';
+import 'package:super_market_application/screens/home.dart';
+import 'package:super_market_application/screens/product.dart';
+import 'package:super_market_application/screens/signin.dart';
+import 'package:super_market_application/screens/signup.dart';
+import 'package:super_market_application/shared/constants.dart';
 
+main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Super Market ',
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        '/signin': (context) => SignIn(),
+        '/signup': (context) => SignUp(),
+        '/editprofile': (context) => EditProfile(),
+        '/products': (context) => ProductView(),
+      },
+      theme: ThemeData(
+        scaffoldBackgroundColor: offWhite,
+      ),
+    );
+  }
+}
