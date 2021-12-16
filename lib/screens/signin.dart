f8712ca685f501bcf14f587ff1c5e917584fa23b
Ammar Abdelmoiz <ammar.abdelmoiz44@gmail.com>
2021-12-16 18:42:25 +0200

    sign

---
+++ lib/screens/signin.dart
@@ -0,0 +1,170 @@
+import 'package:flutter/material.dart';
+import 'package:super_market_application/shared/constants.dart';
+
+class SignIn extends StatefulWidget {
+  @override
+  _SignInState createState() => _SignInState();
+}
+
+class _SignInState extends State<SignIn> {
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              Container(
+                width: 200.0,
+                height: 280.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/logo.png',
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              const Text(
+                'Login to your account',
+                style: TextStyle(
+                  color: green,
+                  fontSize: fontSizeH,
+                  fontWeight: bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: green,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.person_outlined,
+                          color: green,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: green,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock_outlined,
+                          color: green,
+                        ),
+                      ),
+                    ),
+                    Row(
+                      children: [
+                        TextButton(
+                          onPressed: () {},
+                          child: Text(
+                            'Forget Password ?',
+                            style: TextStyle(
+                              fontSize: fontSizeS,
+                              color: black,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 5.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sign in',
+                        style: TextStyle(
+                          fontSize: fontSizeM,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: green,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 35.0, vertical: 15.0),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    const Text(
+                      'or sign in with',
+                      style: TextStyle(
+                        fontSize: fontSizeM,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 10.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Image.asset(
+                      'assets/google.png',
+                      height: 50,
+                    ),
+                    style: btnStyle,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Image.asset(
+                      'assets/facebook.png',
+                      height: 50,
+                    ),
+                    style: btnStyle,
+                  ),
+                  ElevatedButton(
+                    onPressed: () {},
+                    child: Image.asset(
+                      'assets/twitter.png',
+                      height: 50,
+                    ),
+                    style: btnStyle,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 5.0,
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, '/signup');
+                },
+                child: Text(
+                  'Don\'t have an account ? Sign up',
+                  style: TextStyle(
+                    fontSize: fontSizeS,
+                    color: black,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/screens/signup.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:super_market_application/shared/app_bar.dart';
+import 'package:super_market_application/shared/constants.dart';
+
+class SignUp extends StatefulWidget {
+  const SignUp({Key? key}) : super(key: key);
+
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  final _formkey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(50.0),
+        child: TopBar(),
+      ),
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(20.0),
+          child: ListView(
+            children: [
+              Container(
+                width: 200.0,
+                height: 280.0,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage(
+                      'assets/logo.png',
+                    ),
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+              const Text(
+                'Register...',
+                style: TextStyle(
+                  color: green,
+                  fontSize: fontSizeH,
+                  fontWeight: bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              Form(
+                key: _formkey,
+                child: Column(
+                  children: [
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Email',
+                        hintStyle: TextStyle(
+                          color: green,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.person_outlined,
+                          color: green,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    TextFormField(
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Password',
+                        hintStyle: TextStyle(
+                          color: green,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.lock_outlined,
+                          color: green,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 15.0,
+                    ),
+                    TextFormField(
+                      inputFormatters: [
+                        FilteringTextInputFormatter.digitsOnly,
+                      ],
+                      keyboardType: TextInputType.number,
+                      decoration: textInputDecoration.copyWith(
+                        hintText: 'Phone Number',
+                        hintStyle: TextStyle(
+                          color: green,
+                        ),
+                        prefixIcon: Icon(
+                          Icons.phone_android_outlined,
+                          color: green,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10.0,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: Text(
+                        'Sign up',
+                        style: TextStyle(
+                          fontSize: fontSizeM,
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                        primary: green,
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 35.0, vertical: 15.0),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
